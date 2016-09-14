4613850ba7cb9ee584ccf28bdc267670bab7c4da
Priyanshu Agrawal <priyanshuagrawal@outlook.com>
2016-09-14 20:22:03 +0530

    Added Node Class

--- DataStructures.Console/Program.cs
+++ DataStructures.Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataStructures.Core;
 
 namespace DataStructures.Console
 {
@@ -10,6 +11,43 @@ namespace DataStructures.Console
     {
         static void Main(string[] args)
         {
+            // +-----+------+
+            // |  3  | null +
+            // +-----+------+
+            Node first = new Node { Value = 3 };
+
+            // +-----+------+       +-----+------+ 
+            // |  3  | null +       |  5  | null +
+            // +-----+------+       +-----+------+
+            Node middle = new Node { Value = 5 };
+
+            // +-----+------+       +-----+------+ 
+            // |  3  | null +------>|  5  | null +
+            // +-----+------+       +-----+------+
+            first.Next = middle;
+
+            // +-----+------+       +-----+------+      +-----+------+ 
+            // |  3  | null +------>|  5  | null +      |  7  | null +
+            // +-----+------+       +-----+------+      +-----+------+
+            Node last = new Node { Value = 7 };
+
+            // +-----+------+       +-----+------+      +-----+------+ 
+            // |  3  | null +------>|  5  | null +----->|  7  | null +
+            // +-----+------+       +-----+------+      +-----+------+
+            middle.Next = last;
+
+
+            // Iterate over the each node and print the value
+            PrintList(first);
+        }
+
+        static void PrintList(Node node)
+        {
+            while(node != null)
+            {
+                System.Console.WriteLine($"Value: {node.Value}");
+                node = node.Next;
+            }
         }
     }
 }
---
+++ DataStructures.Core/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Core
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+    }
+}
