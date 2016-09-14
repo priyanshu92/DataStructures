f9c0515b157d4c0f6b1e24cc4b478ea80a43a2bf
Priyanshu Agrawal <priyanshuagrawal@outlook.com>
2016-09-14 21:32:16 +0530

    Added Generic Singly Linked List

--- DataStructures.Console/Program.cs
+++ DataStructures.Console/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DataStructures.Core;
+﻿using DataStructures.Core;
 
 namespace DataStructures.Console
 {
---
+++ DataStructures.Core/LinkedListNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Core
+{
+    /// <summary>
+    /// A node in the LinkedList
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class LinkedListNode<T>
+    {
+        /// <summary>
+        /// Creates a new node with specified value.
+        /// </summary>
+        /// <param name="value"></param>
+        public LinkedListNode(T value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// The node value
+        /// </summary>
+        public T Value { get; set; }
+
+        /// <summary>
+        /// A pointer to the next node in the linked list.
+        /// </summary>
+        public LinkedListNode<T> Next { get; set; }
+    }
+}
